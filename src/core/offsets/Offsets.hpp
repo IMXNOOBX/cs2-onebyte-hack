950eca37aa99bb8b90892abb1dd8b0f1e0a20bfb
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2026-05-16 16:32:25 +0200

    Updated to the latest game version

--- src/core/offsets/Offsets.hpp
+++ src/core/offsets/Offsets.hpp
@@ -2,7 +2,6 @@
 namespace offsets
 {
 	// client.dll
-	inline std::ptrdiff_t cvar_unknown = 0xC3DD7A + 0x2;
-	inline std::ptrdiff_t cvar_spec_show_xray = 0xDF6359 + 0x2;
-
+	inline std::ptrdiff_t cvar_unknown = 0xC3F56A + 0x2;
+	inline std::ptrdiff_t cvar_spec_show_xray = 0xDF7B79 + 0x2;
 }
