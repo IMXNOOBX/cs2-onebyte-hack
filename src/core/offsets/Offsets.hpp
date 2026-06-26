1c2d374348d97c9e36bccd9c32d21c8da1ac21d9
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2026-06-26 09:38:37 +0200

    Updated for the latest game version

--- src/core/offsets/Offsets.hpp
+++ src/core/offsets/Offsets.hpp
@@ -1,8 +1,8 @@
 
 namespace offsets
 {
-	// client.dll                                                                                                Addr       Value to modify
-	inline std::ptrdiff_t cvar_unknown = 0xC414FA + 0x2; // If you set any value here, it will override the sig scan, (e.x: 0xC66D2A + 0x2)
+	// client.dll                                                                                                           Addr       Value to modify
+	inline std::ptrdiff_t cvar_unknown = 0xC4149A + 0x2; // If you set any value here, it will override the sig scan, (e.x: 0xC66D2A + 0x2)
 
 	namespace signatures
 	{
