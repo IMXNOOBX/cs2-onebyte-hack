8a174af8f210c8f9ae1a236fe323eebf5d326a9b
raz461 <103881818+raz461@users.noreply.github.com>
2026-09-24 19:45:51 +0200

    fix: update for latest CS2 update

--- src/core/offsets/Offsets.hpp
+++ src/core/offsets/Offsets.hpp
@@ -2,14 +2,14 @@
 namespace offsets
 {
 	// client.dll                                                                                                           Addr       Value to modify
-	inline std::ptrdiff_t cvar_unknown = 0; //0xC4149A + 0x2; // If you set any value here, it will override the sig scan, (e.x: 0xC66D2A + 0x2)
+	inline std::ptrdiff_t cvar_unknown = 0; // A nonzero RVA overrides the signature scan.
 
 	namespace signatures
 	{
-		const std::vector<uint8_t> cvar_unknown = { 
-			0x48, 0x89, 0x5C, 0x24, 0x20,           // mov [rsp+20],rbx
-			0x80, 0x38, 0x00,                       // cmp byte ptr [rax],01
-			0x0F, 0x85, 0x93, 0x00, 0x00, 0x00      // jne
+		const std::vector<uint8_t> cvar_unknown = {
+			0x80, 0x78, 0x58, 0x00,               // cmp byte ptr [rax+58h],0
+			0x0F, 0x85, 0x00, 0x00, 0x00, 0x00,   // jnz (0x00 bytes are wildcards)
+			0x33, 0xC9                          // xor ecx,ecx
 		};
 	}
 }
